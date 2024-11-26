3f53a24eca85c39165f6ddf7e9382494c727f0bc
bottkars <karsten.bott@bottnet.de>
2024-11-26 11:28:15 +0100

    latest

--- lab3/powerstore/replsetup.sh
+++ lab3/powerstore/replsetup.sh
@@ -2,3 +2,5 @@
 wget https://github.com/dell/csm-replication/releases/download/v1.10.0/repctl-linux-amd64
 mv repctl-linux-amd64 ~/.local/bin/repctl
 chmod +X ~/.local/bin/repctl
+chmod 755 ~/.local/bin/repctl
+./repctl cluster add -f ${KUBECONFIG} -n "openshift1-1"
